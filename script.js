06a7564fc2328b7b19ef06adcc7a91b11700170c
jocelymtovars-hub <jocelymtovars@gmail.com>
2026-08-02 21:56:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ sobre.addEventListener("click", () => {
 });
 
 });
+
+const fechaFiesta = new Date("September 12, 2026 20:00:00").getTime();
+
+const contador = setInterval(() => {
+
+const ahora = new Date().getTime();
+
+const diferencia = fechaFiesta - ahora;
+
+
+const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+const horas = Math.floor(
+(diferencia % (1000 * 60 * 60 * 24)) /
+(1000 * 60 * 60)
+);
+
+const minutos = Math.floor(
+(diferencia % (1000 * 60 * 60)) /
+(1000 * 60)
+);
+
+const segundos = Math.floor(
+(diferencia % (1000 * 60)) /
+1000
+);
+
+
+document.getElementById("tiempo").innerHTML =
+dias + " días " +
+horas + " horas " +
+minutos + " minutos " +
+segundos + " segundos";
+
+
+if (diferencia < 0) {
+
+clearInterval(contador);
+
+document.getElementById("tiempo").innerHTML =
+"¡Hoy es el gran día! 🤎🤠";
+
+}
+
+}, 1000);
