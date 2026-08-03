77f6f6927b22593fee3a17ab255f7c5c4aad2367
jocelymtovars-hub <jocelymtovars@gmail.com>
2026-08-02 21:36:07 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const sobre = document.querySelector(".sobre");
+
+sobre.addEventListener("click", () => {
+
+alert("Bienvenido a Jocy's Western Night 🤎");
+
+});
+
+});
