245296b953e75d66a3471e3958700907323beb49
jocelymtovars-hub <jocelymtovars@gmail.com>
2026-08-02 23:19:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const sobre = document.getElementById("sobre");
 const portada = document.getElementById("portada");
 const invitacion = document.getElementById("invitacion");
 
-
 sobre.addEventListener("click", () => {
 
     portada.classList.add("ocultar");
 
     invitacion.classList.add("mostrar");
+
+    const musica = document.getElementById("musica");
+
+    musica.play();
 
 });
 
